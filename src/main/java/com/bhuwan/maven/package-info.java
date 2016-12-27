4a9dfc5dd44a24d1c91488b107cc61b990ca7ca4
bhuwang <bhuwangautam@lftechnology.com>
2016-12-27 16:38:01 +0545

    Added maven info

---
+++ src/main/java/com/bhuwan/maven/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * <pre>
+ * 
+ * Maven is a build tool
+ * Most of the people use maven to manage dependencies especially transitive dependencies.
+ * It handles versioning and releases.
+ * It hepls managing multi-module projects.
+ * It helps generating good project artifacts.
+ * Integration other build tools like Jenkins, Bamboo.
+ * Lots of pre-built plugins available for use.
+ * 
+ * 
+ * </pre>
+ */
+/**
+ * @author bhuwan
+ *
+ */
+package com.bhuwan.maven;
